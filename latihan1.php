d7597abf6effea96900f0c962ac5fcae3587784f
iqbalaulia <miqbalar16@gmail.com>
2019-01-21 21:29:55 +0700

    array assosiatif

---
+++ latihan1.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+$mahasiswa = [
+    "nama" => "Muhammad Iqbal Aulia Rafi",
+    "NIM" => "16410100012",
+    "email" => "miqbalar16@gmail.com"
+];
+
+// Menampilkan array assosiatif
+var_dump($mahasiswa);
+
+?>
