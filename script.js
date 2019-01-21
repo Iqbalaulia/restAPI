e55f8d9d6a4d92cd30799dae964333b74ccdd4bd
iqbalaulia <miqbalar16@gmail.com>
2019-01-21 23:53:05 +0700

    Mengubah JSON jadi Object

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Mengubah Object jadi JSON
 
-let mahasiswa = {
-    nama: "Muhammad Iqbal Aulia Rafi",
-    NIM: "16410100012",
-    email: "miqbalar16@gmail.com"
+// let mahasiswa = {
+//     nama: "Muhammad Iqbal Aulia Rafi",
+//     NIM: "16410100012",
+//     email: "miqbalar16@gmail.com"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+// ====================================
+
+// JSON ke Object
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        let mahasiswa = JSON.parse(this.responseText);
+        console.log(mahasiswa);
+    }
 }
 
-console.log(JSON.stringify(mahasiswa));
+// menjalankan Ajax
+xhr.open('GET', 'coba.json', 'true');
+xhr.send();
