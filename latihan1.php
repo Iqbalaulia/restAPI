ccd4aabc796207e0235c4e7ed1428a8b480eecc8
iqbalaulia <miqbalar16@gmail.com>
2019-01-21 22:22:25 +0700

    Connect DB dengan JSON

--- latihan1.php
+++ latihan1.php
@@ -1,17 +1,39 @@
 <?php 
 
 
-$mahasiswa = [
-    "nama" => "Muhammad Iqbal Aulia Rafi",
-    "NIM" => "16410100012",
-    "email" => "miqbalar16@gmail.com"
-];
+// $mahasiswa = [
+//     [
+//         "nama" => "Muhammad Iqbal Aulia Rafi",
+//         "NIM" => "16410100012",
+//         "email" => "miqbalar16@gmail.com"
+
+//     ],
+//     [
+//         "nama" => "Indana Nazulfa",
+//         "NIM" => "18410100006",
+//         "email" => "miqbalar16@gmail.com"
+
+//     ]
+
+// ];
+
+
+
 
 // Menampilkan array assosiatif
 // var_dump($mahasiswa);
 
 
+// Menampilkan isi database -> table mahasiswa menggunakan PDO dan JSON
+
+$dbh = new PDO('mysql:host=localhost;dbname=db_ahp', 'root', 'sepatusandal12');
+
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+
 // Menampilkan array assosiatif dalam bentuk JSON
 $data = json_encode($mahasiswa);
 echo $data;
-?>
+?>  
