31417dd062e470a9f775eeb203c9cf59afc687a1
iqbalaulia <miqbalar16@gmail.com>
2019-01-22 00:02:59 +0700

    Jquery JSON

--- script.js
+++ script.js
@@ -11,14 +11,21 @@
 // ====================================
 
 // JSON ke Object
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        let mahasiswa = JSON.parse(this.responseText);
-        console.log(mahasiswa);
-    }
-}
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa);
+//     }
+// }
 
-// menjalankan Ajax
-xhr.open('GET', 'coba.json', 'true');
-xhr.send();
+// // menjalankan Ajax
+// xhr.open('GET', 'coba.json', 'true');
+// xhr.send();
+
+
+
+// ======================================
+$.getJSON("coba.json", function (data, textStatus, jqXHR) {
+    console.log(data);
+});
